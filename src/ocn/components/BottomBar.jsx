aa78c9002372cf7c7f5c20231bd25028c01ac9c4
Henry Li <henry1943@163.com>
2017-04-11 20:07:50 +0800

    Update

--- src/ocn/components/BottomBar.jsx
+++ src/ocn/components/BottomBar.jsx
@@ -6,6 +6,7 @@ export default class BottomBar extends React.PureComponent {
   render() {
     return (
       <div className="ocn-bottom-bar">
+        <div className="hint">请选择看过的影片</div>
         <button className="next-batch" onClick={this.props.onNextBatchButtonClick}>换一批</button>
       </div>
     );
