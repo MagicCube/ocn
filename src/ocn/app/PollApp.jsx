ad5f5ff4f0221632dac4d470c0e76feb561e9cb3
Henry Li <henry1943@163.com>
2017-04-12 16:34:04 +0800

    Update

--- src/ocn/app/PollApp.jsx
+++ src/ocn/app/PollApp.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 
 import BottomBar from '../components/BottomBar';
 import MovieCoverList from '../components/MovieCoverList';
-import { nextBatch } from '../logic/poll-service';
+import * as service from '../logic/poll-service';
 
 import '../index.html';
 import '../res/index.less';
@@ -23,14 +23,37 @@ export default class PollApp extends React.PureComponent {
 
   async nextBatch() {
     document.body.scrollTop = 0;
-    const movies = await nextBatch();
+    const movies = await service.nextBatch();
     this.setState({ movies: movies.slice(0, 20) });
+  }
+
+  async selectMovie(movie) {
+    movie.selected = true;
+    this.forceUpdate();
+    await service.selectMovie(movie.id);
+  }
+
+  async deselectMovie(movie) {
+    movie.selected = false;
+    this.forceUpdate();
+    await service.deselectMovie(movie.id);
+  }
+
+  handleMovieClick(movie) {
+    if (!movie.selected) {
+      this.selectMovie(movie);
+    } else {
+      this.deselectMovie(movie);
+    }
   }
 
   render() {
     return (
       <div className="ocn-poll-app">
-        <MovieCoverList data={this.state.movies ? this.state.movies : []} />
+        <MovieCoverList
+          data={this.state.movies ? this.state.movies : []}
+          onMovieClick={movie => this.handleMovieClick(movie)}
+        />
         <BottomBar onNextBatchButtonClick={() => this.nextBatch()} />
       </div>
     );
